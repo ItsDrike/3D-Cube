5bf0b35343e5b06e7b7f722a1553547acf3b2d0d
ItsDrike <itsdrikeofficial@gmail.com>
2020-09-05 23:47:24 +0200

    Initial Commit

---
+++ src/__main__.py
@@ -0,0 +1,67 @@
+from contextlib import suppress
+
+import pygame
+
+from src.util import Colors
+
+WIDTH, HEIGHT = 1200, 900
+TICK_RATE = 100
+
+
+class Game:
+    def __init__(self, width: int, height: int, fps: int) -> None:
+        self.size = self.width, self.height = width, height
+
+        pygame.init()
+        self.screen = pygame.display.set_mode(self.size)
+        self.fps_clock = pygame.time.Clock()
+        self.tick_rate = fps
+
+        self.running = True
+
+    def handle_user_event(self) -> None:
+        """Handle pygame events (f.e.: quit, click)."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                self.running = False
+
+    def redraw_screen(self) -> None:
+        """
+        Redraw all cells on the screen.
+
+        This does not update the screen, it only redraws it.
+        """
+        self.screen.fill(Colors.GREY)
+
+        # TODO: Main drawing
+
+    def update_screen(self, tick: bool = True) -> None:
+        """
+        Update the screen accordingly to `redraw_screen`
+        also check for user event and tick (if not specified otherwise)
+        """
+
+        self.handle_user_event()
+
+        if not self.running:
+            return
+
+        self.redraw_screen()
+        pygame.display.update()
+        if tick:
+            self.fps_clock.tick(self.tick_rate)
+
+    def main(self) -> None:
+        # Main game loop
+        while self.running:
+            self.update_screen()
+
+
+game = Game(WIDTH, HEIGHT, TICK_RATE)
+
+with suppress(KeyboardInterrupt):
+    game.main()
+
+print("\nStopped")
+pygame.quit()
---
+++ src/util.py
@@ -0,0 +1,28 @@
+import random
+import typing as t
+
+
+def number_remap(
+    value: float,
+    old_min: float,
+    old_max: float,
+    new_min: float,
+    new_max: float
+) -> float:
+    return ((value - old_min) / (old_max - old_min)) * (new_max - new_min) + new_min
+
+
+class Colors:
+    BLACK = 0, 0, 0
+    WHITE = 255, 255, 255
+    GREY = 125, 125, 125
+    RED = 240, 20, 30
+    GREEN = 30, 255, 20
+    BLUE = 100, 0, 255
+    YELLOW = 255, 255, 0
+
+    ColorType = t.Tuple[int, int, int]
+
+    @staticmethod
+    def random() -> ColorType:
+        return (random.randint(100, 255), 0, random.randint(100, 255))
